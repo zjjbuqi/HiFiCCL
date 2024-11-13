1e9b8051b6d724e15d992aa12fd39456cdd0dcfb
zjjbuqi <jzj1017629735@163.com>
2024-11-13 15:32:05 +0800

    Add files via upload

--- hificcl_PLG.py
+++ hificcl_PLG.py
@@ -1,7 +1,5 @@
 import collections
 import re
-
-
 class PangenomeLabelGraph:
     def __init__(self):
         self.node_label = {}
@@ -22,19 +20,10 @@ class PangenomeLabelGraph:
             self.graph[source][target] = weight
             self.graph_reverse[target][source] = weight
 
-
-    # def add_all_node(self, input_dict):
-    #     for key in input_dict:
-    #         self.add_vertex(key)
-    #         self.node_label[key] = {'label':[], 'visited':0}
-
     def gfa_processed(self, pangfa):
         with open(pangfa, 'r') as f:
             for line in f:
-                # if len(line.split())<7:
-                #     continue
                 head_char = line.split()[:7][0]
-                # adding segment_chr information
                 if head_char == 'S':
                     segmentid = line.split()[:7][1]
                     self.add_vertex(segmentid)
@@ -45,8 +34,7 @@ class PangenomeLabelGraph:
                         if match:
                             result = match.group()
                             chrIn = result
-                        # else:
-                        #     chrIn = "None"
+
                     self.node_label[segmentid] = {'label': chrIn, 'visited': 0}
                     if chrIn.startswith("HG") and chrIn not in self.label_to_seg:
                         self.label_to_seg[chrIn] = segmentid
@@ -64,7 +52,6 @@ class PangenomeLabelGraph:
         end = 0
         visited = set()
         while (end != 1):
-            # try:
             iter_data = iter(self.graph_reverse[start])
             while (1):
                 up_key = next(iter_data, None)
@@ -119,27 +106,24 @@ class PangenomeLabelGraph:
             if node_label[node]['label'][0].startswith('chr') and node_label[node]['label'][0] in self.chrid:
                 continue
             elif node_label[node]['label'][0].startswith('HG') or node_label[node]['label'][0].startswith('NA') or node_label[node]['label'][0].startswith('chr'):
-                # print(f"This run {node_label[node]['label'][0]}!")
                 if node_label[node]['label'][0] in self.label_to_chr:
                     node_label[node]['label'][0] =  self.label_to_chr[node_label[node]['label'][0]]
-                    # print(f"Transform is {node_label[node]['label'][0]}")
                 else:
                     flag = self.Findprimary_up(node_label[node]['label'][0])
                     if flag.startswith('chr') and flag in self.chrid:
                         if node_label[node]['label'][0] not in self.label_to_chr:
                             self.label_to_chr[node_label[node]['label'][0]] = flag
                         node_label[node]['label'][0] = flag
-                        # print(f"Transform is {node_label[node]['label'][0]}")
+
                     else:
                         flag2 = self.Findprimary_down(node_label[node]['label'][0], flag)
                         if flag2.startswith('chr') and flag2 in self.chrid:
                             if node_label[node]['label'][0] not in self.label_to_chr:
                                 self.label_to_chr[node_label[node]['label'][0]] = flag2
                             node_label[node]['label'][0] = flag2
-                            # print(f"Transform is {node_label[node]['label'][0]}")
                         else:
                             node_label[node]['label'][0] = 'None'
-                            # print(f"Transform is {node_label[node]['label'][0]}")
+
         print(f"There are {self.testc} None!")
 
     def build_PLG(self, pangfa):
